f659dcc92148e120de3b3f809a1d41b02ea1cab5
Vaidas <vaidas.makstutis@gmail.com>
2023-01-24 11:36:12 +0200

    Function get countries from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { TCountry } from "./api";
+import { API_URL } from "./api/shared/constants";
+import Countries from "./Components/Countries";
+
 const App = () => {
+  const [countries, setCountries] = useState<TCountry[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      setLoading(true);
+      const res = await axios.get(API_URL);
+      const data: TCountry[] = res.data;
+      setCountries([...data]);
+      setLoading(false);
+    };
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
-      <h2>Test Assignment!</h2>
+      <section className="countries-header">
+        <h2>Countries list</h2>
+      </section>
+      <Countries countries={countries} loading={loading} />
     </div>
   );
 };
