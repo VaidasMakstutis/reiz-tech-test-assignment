9af4819360b870ff5126d8f7a000d6c55d0190ba
Vaidas <vaidas.makstutis@gmail.com>
2023-01-28 19:20:51 +0200

    Filter by area from select dropdown

--- src/Components/Filter/Filter.tsx
+++ src/Components/Filter/Filter.tsx
@@ -5,32 +5,38 @@ import { TCountry } from "../../api";
 type IFilterProps = {
   countries: TCountry[];
   setShowCountries: React.Dispatch<React.SetStateAction<TCountry[]>>;
-  areaRef: React.MutableRefObject<HTMLButtonElement | null>;
-  activeButton: string;
-  setActiveButton: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const Filter = ({ countries, setShowCountries, areaRef, activeButton, setActiveButton }: IFilterProps) => {
+const Filter = ({ countries, setShowCountries }: IFilterProps) => {
   const [filterByRegion, setFilterByRegion] = useState("");
+  const [filterByArea, setFilterByArea] = useState(0);
 
-  const findCountriesSmallerThanLtu = () => {
-    const areaOfLtu = 65300;
-    setShowCountries(countries.filter(country => country.area < areaOfLtu));
-    setActiveButton(areaRef?.current?.id || "");
+  const handleChangeByArea: React.ChangeEventHandler<HTMLSelectElement> = event => {
+    setFilterByArea(parseInt(event.target.value));
+    if (parseInt(event.target.value) === 0) {
+      setShowCountries(countries);
+    } else {
+      setShowCountries(countries.filter(country => country.area < parseInt(event.target.value)));
+    }
   };
 
-  const handleChange: React.ChangeEventHandler<HTMLSelectElement> = event => {
+  const handleChangeByRegion: React.ChangeEventHandler<HTMLSelectElement> = event => {
     setFilterByRegion(event.target.value);
     setShowCountries(() => countries.filter(country => country.region.includes(event.target.value)));
   };
 
   return (
-    <div className="filter-options d-flex inline-flex">
-      <button ref={areaRef} id="filter-area" type="button" onClick={findCountriesSmallerThanLtu}>
-        Countries are smaller than Lithuania
-      </button>
+    <div className="filter-options d-flex">
       <div>
-        <Form.Select aria-label="Default select example" value={filterByRegion} onChange={handleChange}>
+        <Form.Select aria-label="Default select example" value={filterByArea} onChange={handleChangeByArea}>
+          <option value="0">Filter countries by area...</option>
+          <option value="65300">Smaller than Lithuania</option>
+          <option value="881912">Smaller than Pakistan</option>
+          <option value="131990">Smaller than Greece</option>
+        </Form.Select>
+      </div>
+      <div>
+        <Form.Select aria-label="Default select example" value={filterByRegion} onChange={handleChangeByRegion}>
           <option value="">Filter countries by region...</option>
           <option value="Americas">Americas</option>
           <option value="Asia">Asia</option>
